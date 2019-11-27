7eb086d659a6c576f9b485a2b8c4647f46c1b24e
Uttam K Ukkoji <uttamukkoji@gmail.com>
2019-11-27 18:28:05 +0530

    Organization test case completed

--- test/api/organization-test.js
+++ test/api/organization-test.js
@@ -15,80 +15,80 @@ describe('Organization api test', () => {
     organization = client.organization(orgID)
   })
 
-  // it('Fetch all organizations', done => {
-  //   client.organization().fetchAll()
-  //     .then((response) => {
-  //       for (const index in response.items) {
-  //         const organization = response.items[index]
-  //         expect(organization.name).to.not.equal(null, 'Organization name cannot be null')
-  //         expect(organization.uid).to.not.equal(null, 'Organization uid cannot be null')
-  //       }
-  //       done()
-  //     })
-  //     .catch((error) => {
-  //       console.log(error)
-  //       expect(error).to.be.equal(null, 'Failed Organization call find all.')
-  //       done()
-  //     })
-  // })
+  it('Fetch all organizations', done => {
+    client.organization().fetchAll()
+      .then((response) => {
+        for (const index in response.items) {
+          const organization = response.items[index]
+          expect(organization.name).to.not.equal(null, 'Organization name cannot be null')
+          expect(organization.uid).to.not.equal(null, 'Organization uid cannot be null')
+        }
+        done()
+      })
+      .catch((error) => {
+        console.log(error)
+        expect(error).to.be.equal(null, 'Failed Organization call find all.')
+        done()
+      })
+  })
 
-  // it('Find One organizations', done => {
-  //   organization.fetch()
-  //     .then((organization) => {
-  //       console.log(organization)
-  //       expect(organization.name).to.be.equal('Contentstack Employees', 'Organization name dose not match')
-  //       done()
-  //     })
-  //     .catch((error) => {
-  //       console.log(error)
-  //       expect(error).to.be.equal(null, 'Failed Organization call find one.')
-  //       done()
-  //     })
-  // })
+  it('Find One organizations', done => {
+    organization.fetch()
+      .then((organization) => {
+        console.log(organization)
+        expect(organization.name).to.be.equal('Contentstack Employees', 'Organization name dose not match')
+        done()
+      })
+      .catch((error) => {
+        console.log(error)
+        expect(error).to.be.equal(null, 'Failed Organization call find one.')
+        done()
+      })
+  })
 
-  // it('Get all stacks in an Organization', done => {
-  //   organization.stacks()
-  //     .then((response) => {
-  //       for (const index in response.items) {
-  //         const stack = response.items[index]
-  //         expect(stack.name).to.not.equal(null, 'Organization name cannot be null')
-  //         expect(stack.uid).to.not.equal(null, 'Organization uid cannot be null')
-  //         expect(stack.org_uid).to.equal('orgID', 'Organization uid cannot be null')
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.log(error)
-  //       expect(error).to.be.equal(null, 'Failed Organization call find one.')
-  //       done()
-  //     })
-  // })
+  it('Get all stacks in an Organization', done => {
+    organization.stacks()
+      .then((response) => {
+        for (const index in response.items) {
+          const stack = response.items[index]
+          expect(stack.name).to.not.equal(null, 'Organization name cannot be null')
+          expect(stack.uid).to.not.equal(null, 'Organization uid cannot be null')
+          expect(stack.org_uid).to.equal('orgID', 'Organization uid cannot be null')
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+        expect(error).to.be.equal(null, 'Failed Organization call find one.')
+        done()
+      })
+  })
 
-  // it('Transfer Organization Ownership', done => {
-  //   organization.transferOwnership('em@em.com')
-  //     .then((notice) => expect(notice).to.be.equal('Email has been successfully sent to the user.', 'Message does not match'))
-  //     .catch((error) => {
-  //       console.log(error)
-  //       expect(error).to.be.equal(null, 'Failed Transfer Organization Ownership')
-  //       done()
-  //     })
-  // })
+  it('Transfer Organization Ownership', done => {
+    organization.transferOwnership('em@em.com')
+      .then((notice) => expect(notice).to.be.equal('Email has been successfully sent to the user.', 'Message does not match'))
+      .catch((error) => {
+        console.log(error)
+        expect(error).to.be.equal(null, 'Failed Transfer Organization Ownership')
+        done()
+      })
+  })
 
-  // it('Get all roles in an organization', done => {
-  //   organization.roles()
-  //     .then((roles) => {
-  //       for (const i in roles.items) {
-  //         expect(roles.items[i].uid).to.not.equal(null, 'Role uid cannot be null')
-  //         expect(roles.items[i].name).to.not.equal(null, 'Role name cannot be null')
-  //         expect(roles.items[i].org_uid).to.be.equal(orgID, 'Role org_uid not match')
-  //       }
-  //       done()
-  //     })
-  //     .catch((error) => {
-  //       console.log(error)
-  //       expect(error).to.be.equal(null, 'Failed Transfer Organization Ownership')
-  //       done()
-  //     })
-  // })
+  it('Get all roles in an organization', done => {
+    organization.roles()
+      .then((roles) => {
+        for (const i in roles.items) {
+          expect(roles.items[i].uid).to.not.equal(null, 'Role uid cannot be null')
+          expect(roles.items[i].name).to.not.equal(null, 'Role name cannot be null')
+          expect(roles.items[i].org_uid).to.be.equal(orgID, 'Role org_uid not match')
+        }
+        done()
+      })
+      .catch((error) => {
+        console.log(error)
+        expect(error).to.be.equal(null, 'Failed Transfer Organization Ownership')
+        done()
+      })
+  })
 
   it('Get all invitations in an organization', done => {
     organization.getInvitations({ include_count: true })
