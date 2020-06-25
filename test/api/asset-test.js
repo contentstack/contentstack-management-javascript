d4bebc22db6df458462ee2e9dd0df5a9a2bb6682
Uttam K Ukkoji <uttamukkoji@gmail.com>
2020-06-25 16:59:23 +0530

    Extension functionality added
    - Query Count method added
    - Extension implementation and test case added
    - content type. entry, global field, locale, roles, asset Documentation updated

--- test/api/asset-test.js
+++ test/api/asset-test.js
@@ -1,21 +1,26 @@
 import { describe, it, setup } from 'mocha'
 import * as contentstack from '../../lib/contentstack.js'
 import axios from 'axios'
+import { jsonReader } from '../utility/fileOperations/readwrite'
 
 var client = {}
+var stack = {}
 
 describe('ContentType api Test', () => {
   setup(() => {
-    client = contentstack.client(axios, { authtoken: 'bltfa472b464438dee2' })
+    const user = jsonReader('loggedinuser.json')
+
+    stack = jsonReader('stack.json')
+    client = contentstack.client(axios, { authtoken: user.authtoken })
   })
 
   it('test asset Upload ', done => {
-    const asset = {
-      upload: '/Users/uttamukkoji/Documents/JS/contentstack/Modular/assets/blt85b501e49352272c/mobile_ad_campaign_0.gif'
-    }
-    client.stack('blt3490707e04b3febd').asset().create(asset)
+    // const asset = {
+    //   upload: '/Users/uttamukkoji/Documents/JS/contentstack/Modular/assets/blt85b501e49352272c/mobile_ad_campaign_0.gif'
+    // }
+    client.stack(stack.api_key).asset().query({ include_folders: true, query: { is_dict: true } }).count()
       .then((asset) => {
-        console.log(asset.uid)
+        console.log(asset)
         done()
       })
       .catch(done)
