a61d87ac924b2ac075aa53d5ac3fa64cfbee6406
nadeem-cs <nadeem.patwekar@contentstack.com>
2023-04-03 13:25:58 +0530

    feat: :construction: merge functionality added [CS-36688]

--- types/stack/branch/index.d.ts
+++ types/stack/branch/index.d.ts
@@ -1,3 +1,4 @@
+import { Response } from "../../contentstackCollection";
 import { AnyProperty, SystemFields } from "../../utility/fields";
 import { Queryable, SystemFunction } from "../../utility/operations";
 import { Compare } from './compare';
@@ -7,9 +8,27 @@ export interface Branch extends SystemFields, SystemFunction<Branch> {
 }
     
 export interface Branches extends Queryable<Branch, {branch: BranchData}> {
+    merge(mergeObj: MergePayload): Promise<AnyProperty>
 }
 
 export interface BranchData extends AnyProperty {
     uid: string
     source: string
 }
+
+export interface MergePayload extends AnyProperty {
+    base_branch: string
+    compare_branch: string
+    default_merge_strategy: MergeStrategy
+    item_merge_strategies?: ItemMergeStrategy[]
+    merge_comment: string
+    no_revert?: boolean
+}
+
+export interface ItemMergeStrategy extends AnyProperty {
+    uid: string
+    type: string
+    merge_strategy: MergeStrategy     
+}
+
+export type MergeStrategy = "merge_prefer_base" | "merge_prefer_compare" | "overwrite_with_compare" | "merge_new_only" | "merge_modified_only_prefer_base" | "merge_modified_only_prefer_compare" | "ignore"
