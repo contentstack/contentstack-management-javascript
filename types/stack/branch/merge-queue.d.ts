b5a4514fb9a5050a037f1551bfc759681d3d9383
nadeem-cs <nadeem.patwekar@contentstack.com>
2023-04-11 17:08:14 +0530

    feat: :sparkles: added compare and merge functionality for branches with unit test cases [CS-36688]
    A new branch is created for the feature as the prev branch `CS-36487-branch-compare-merge` encountered many merge conflicts.

---
+++ types/stack/branch/merge-queue.d.ts
@@ -0,0 +1,10 @@
+import { Response } from "../../contentstackCollection"
+import { AnyProperty } from "../../utility/fields"
+
+export interface MergeQueue {
+  fetch():Promise<AnyProperty>
+}
+
+export interface MergeQueues {
+  find(params?: object): Promise<AnyProperty>
+}
