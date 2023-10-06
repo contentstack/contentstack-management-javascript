fd868c4fe748e0f6e5fb419fead902199d778888
harshithad0703 <harshitha.d@contentstack.com>
2023-10-06 20:41:45 +0530

    feat: :sparkles: taxonomy and terms implementation for typescript

---
+++ types/stack/taxonomy/index.d.ts
@@ -0,0 +1,22 @@
+import { AnyProperty, SystemFields } from "../../utility/fields";
+import { Creatable, Queryable, SystemFunction } from "../../utility/operations";
+import { Term, Terms } from "../taxonomy/terms"
+
+export interface Taxonomy extends SystemFields, SystemFunction<Taxonomy> {
+    term(): Terms
+    term(uid: string): Term
+}
+
+export interface Taxonomy extends SystemFields, SystemFunction<Taxonomy> {
+}
+    
+export interface Taxonomies extends Queryable<Taxonomy, {taxonomy: TaxonomyData}> {
+}
+
+export interface Taxonomies extends Creatable<Taxonomy, {taxonomy: TaxonomyData}> {
+}
+
+export interface TaxonomyData extends AnyProperty {
+    name: string
+    taxonomy_uid: string
+}
