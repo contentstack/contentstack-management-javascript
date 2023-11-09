0aa565d3d2d582fd0c7635b2a2832b86cf52010b
harshithad0703 <harshitha.d@contentstack.com>
2023-11-09 18:55:18 +0530

    types support for stackRoleMapping and test cases

--- types/teams/stackRoleMappings/index.d.ts
+++ types/teams/stackRoleMappings/index.d.ts
@@ -2,16 +2,17 @@ import { AnyProperty, SystemFields } from "../../utility/fields";
 import { ContentstackCollection } from '../../contentstackCollection'
 import { SystemFunction } from "../../utility/operations";
 
-export interface StackRoleMapping extends SystemFields, SystemFunction<StackRoleMapping> {
-    update(data: { stackRoleMapping: StackRoleMappingData }): Promise<StackRoleMapping>
+export interface StackRoleMapping extends StackRoleMappingData {
+    update(data:StackRoleMappingData): Promise<{StackRoleMappingData: StackRoleMappingData}>
 }
 
-export interface StackRoleMappings {
+export interface StackRoleMappings extends StackRoleMappingData {
     fetchAll(params?: AnyProperty): Promise<ContentstackCollection<StackRoleMappings>>
-    add(data: { stackRoleMapping: StackRoleMappingData }): Promise<StackRoleMapping>
+    add(data: StackRoleMappingData): Promise<StackRoleMappings>
 }
 
 export interface StackRoleMappingData extends AnyProperty {
     stackApiKey?: string,
     roles: string[]
 }
+
