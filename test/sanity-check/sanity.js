258b3a9128c322a63c3c6e2460b8798474f755e9
harshithad0703 <harshitha.d@contentstack.com>
2024-01-05 11:54:27 +0530

    test: added branches and aliases test suits to sanity folder

--- test/sanity-check/sanity.js
+++ test/sanity-check/sanity.js
@@ -1,6 +1,8 @@
 require('./api/user-test')
 require('./api/organization-test')
 require('./api/stack-test')
+require('./api/branch-test')
+require('./api/branchAlias-test')
 require('./api/contentType-test')
 require('./api/asset-test')
 require('./api/entry-test')
