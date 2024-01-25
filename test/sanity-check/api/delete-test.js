6938c0d76f7ca1f1340f753021c5f88656d6ee67
sunil-lakshman <104969541+sunil-lakshman@users.noreply.github.com>
2024-01-25 13:56:10 +0530

    Feat/cs 43619 taxonomy import export testcases (#118)
    * Next (#115)
    
    * test: Added locale test suit and delete test suit to sanity folder
    
    * chore: updated package-lock file
    
    * Added taxonomy import/export support (#112)
    
    Co-authored-by: Nadeem <110535104+nadeem-cs@users.noreply.github.com>
    
    * Validating the object  when update the entries with assets (#114)
    
    Co-authored-by: Nadeem <110535104+nadeem-cs@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: harshithad0703 <harshitha.d@contentstack.com>
    Co-authored-by: harshithad0703 <104908717+harshithad0703@users.noreply.github.com>
    Co-authored-by: Nadeem <110535104+nadeem-cs@users.noreply.github.com>
    
    * Added taxonomy import/export testcases.
    
    ---------
    
    Co-authored-by: Nadeem <110535104+nadeem-cs@users.noreply.github.com>
    Co-authored-by: harshithad0703 <harshitha.d@contentstack.com>
    Co-authored-by: harshithad0703 <104908717+harshithad0703@users.noreply.github.com>

--- test/sanity-check/api/delete-test.js
+++ test/sanity-check/api/delete-test.js
@@ -121,6 +121,7 @@ describe('Branch Alias delete api Test', () => {
       .catch(done)
   })
 })
+
 function makeEnvironment (uid = null) {
   return client.stack({ api_key: process.env.API_KEY }).environment(uid)
 }
@@ -136,3 +137,4 @@ function makeDeliveryToken (uid = null) {
 function makeBranchAlias (uid = null) {
   return client.stack({ api_key: process.env.API_KEY }).branchAlias(uid)
 }
+
