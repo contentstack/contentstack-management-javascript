463e284002fc4a8b02a232cbc1214faf15031a54
harshitha.d <harshitha.d@contentstack.com>
2025-02-07 16:39:35 +0530

    add and update test cases for api_version support

--- test/sanity-check/mock/variantEntry.js
+++ test/sanity-check/mock/variantEntry.js
@@ -6,6 +6,44 @@ const variantEntryFirst = {
       _change_set: ["title", "url"],
     },
   },
-}
+};
 
-export { variantEntryFirst };
+var publishVariantEntryFirst = {
+  entry: {
+    environments: ["development"],
+    locales: ["en-us", "en-at"],
+    variants: [
+      {
+        uid: "",
+        version: 1,
+      },
+    ],
+    variant_rules: {
+      publish_latest_base: false,
+      publish_latest_base_conditionally: true,
+    },
+  },
+  locale: "en-us",
+  version: 1,
+};
+
+const unpublishVariantEntryFirst = {
+  entry: {
+    environments: ["development"],
+    locales: ["en-at"],
+    variants: [
+      {
+        uid: "",
+        version: 1,
+      },
+    ],
+    variant_rules: {
+      publish_latest_base: false,
+      publish_latest_base_conditionally: true,
+    },
+  },
+  locale: "en-us",
+  version: 1,
+};
+
+export { variantEntryFirst, publishVariantEntryFirst, unpublishVariantEntryFirst };
