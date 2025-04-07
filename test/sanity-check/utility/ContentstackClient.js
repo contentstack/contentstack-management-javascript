1db6cc7aaeacf824dc9e60ec59e4b227657877a3
harshitha.d <harshitha.d@contentstack.com>
2025-04-07 13:07:23 +0530

    enh: Add environment variable checks for ContentstackClient

--- test/sanity-check/utility/ContentstackClient.js
+++ test/sanity-check/utility/ContentstackClient.js
@@ -1,6 +1,15 @@
 import * as contentstack from '../../../lib/contentstack.js'
 import dotenv from 'dotenv'
 dotenv.config()
+
+const requiredVars = ['HOST', 'EMAIL', 'PASSWORD', 'ORGANIZATION', 'API_KEY']
+const missingVars = requiredVars.filter((key) => !process.env[key])
+
+if (missingVars.length > 0) {
+  console.error(`\x1b[31mError: Missing environment variables - ${missingVars.join(', ')}`)
+  process.exit(1)
+}
+
 function contentstackClient (authtoken = null) {
   var params = { host: process.env.HOST, defaultHostName: process.env.DEFAULTHOST }
   if (authtoken) {
